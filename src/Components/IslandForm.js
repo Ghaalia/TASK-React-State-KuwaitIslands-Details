45ddc3819dd86db280f4317e393e5769a8948385
Ghaalia <Ghaliaals9@gmail.com>
2023-10-16 00:28:24 +0300

    to be continue ....

--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,26 @@
 export default function IslandForm({ island }) {
+  const getUserInputs = () => {
+    let userName = document.getElementById("name").value;
+    let userPhone = document.getElementById("phone").value;
+
+    // return confirm(
+    //   `Are you sure you want to book to ${island.name} with the Name: ${userName}, phone: ${userPhone}`
+    // );
+  };
+
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input placeholder="Type Full Name" type="text" id="name" />
+      <input placeholder="Type Phone Number" type="phone" id="phone" />
+      <button
+        className="book"
+        onClick={() => {
+          getUserInputs();
+        }}
+      >
         Book for today!
       </button>
     </div>
